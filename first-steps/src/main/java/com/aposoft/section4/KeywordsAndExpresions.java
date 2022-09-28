e20c476c42bee4746781fa84d2fcc21039f74576
uhlarik <uhlarik.stanislav@gmail.com>
2022-09-28 20:21:51 +0200

    Init commit

--- first-steps/src/main/java/com/aposoft/section4/KeywordsAndExpresions.java
+++ first-steps/src/main/java/com/aposoft/section4/KeywordsAndExpresions.java
@@ -66,4 +66,17 @@ public class KeywordsAndExpresions {
         return 4;
     }
 
+    // method overloading
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored "+ score + " points");
+        return score *1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed Player scored "+ score + " points");
+        return score *1000;
+    }
+
+    
+
 }
